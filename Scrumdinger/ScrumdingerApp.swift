5494e9742d900764a109957d851512f3c22ce83e
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-06-26 18:36:53 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hamza Rafique Azad on 26/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
